63bc0efed7bc277f8399c15e1a3771c76cabb595
MakerSquare Student <students@themakersquare.com>
2013-10-04 10:07:05 -0500

    Add model that updates stock object with a price property.

--- src/models/stock.js
+++ src/models/stock.js
@@ -1,9 +1,12 @@
+
+
 (function () {
 
   window.Stock = Backbone.Model.extend({
     updatePrice: function (newPrice) {
-      console.log('Updating', this.get('name'), 'price to:', newPrice);
-      // TODO
+      // console.log('Updating', this.get('name'), 'price to:', newPrice);
+      this.set({price: parseFloat(newPrice)});
+      console.log('price:', this.get('price'));
     }
   });
 
